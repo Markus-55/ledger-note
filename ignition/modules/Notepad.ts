03c26b9b80c82a081f02dbbf7b2dbffef4a17a91
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-09-06 12:50:00 +0900

    Remove unused owner variable in Nodepad Module

--- ignition/modules/Notepad.ts
+++ ignition/modules/Notepad.ts
@@ -2,8 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const hre = require("hardhat");
 
 const NotepadModule = buildModule("NotepadModule", (m) => {
-  const owner = m.getAccount(0);
-  const notepad = m.contract("Notepad", [owner], {});
+  const notepad = m.contract("Notepad");
 
   return { notepad };
 });
